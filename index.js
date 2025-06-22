3a4ec71393038b7c6b48612e25c8489a28e7e444
Oladeji <oladeji.okuneye@gmail.com>
2025-06-23 00:29:55 +0100

    Modified the index.js file with logging request

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-// Example: index.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {"\n     res.send('Hello World!');\n   "});
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
